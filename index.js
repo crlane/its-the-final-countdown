df248151e97f150ad7d41ded29fc808375a2dde1
Cameron Lane <crlane@adamanteus.com>
2019-11-12 23:01:56 +0000

    fixup function name

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 'use strict';
 const querystring = require('querystring');
 
-exports.http = (request, response) => {
-  var err = '<h2>Invalid date</h2><p>Only Accepts YYYY-MM-DD or a number in the range [0, 1000000]</p>'
+var conversionFactor = 24 * 60 * 60 * 1000;
+var daysToMillis = (days) => {
+  return days * conversionFactor;
+};
+
+var millisToDays = (millis) => {
+  return millis / conversionFactor;
+};
+exports.countdown = (request, response) => {
+  var err = '<h2>Invalid date</h2><p>Only Accepts YYYY-MM-DD or a postive number of days</p>'
   var date1 = Date.now();
   var default_string = "2020-10-31";
   var q_date = request.query.q || default_string
   if (isNaN(q_date)) {
     q_date = querystring.escape(q_date)
-  } else if (q_date < 0 || q_date > 1000000) {
-    // number, milliseconds since the epoch UTC
+  } else if (q_date < 0) {
     response.status(400).send(err);
     return;
+  } else {
+    // number, milliseconds since the epoch UTC, convert to this many milliseconds in the future
+    var epoch = new Date(0);
+    q_date = (date1 - epoch) + daysToMillis(q_date);
   }
   var date2 = new Date(q_date);
   if (date2 == 'Invalid Date') {
     response.status(400).send(err);
     return;
   }
-  var diffDays = parseInt((date2 - date1) / (1000 * 60 * 60 * 24));
-  response.status(200).send('<h2>Days: ' + diffDays + '</h2>');
+  var diffDays = parseInt(millisToDays(date2 - date1));
+  response.status(200).send('<h2>' + diffDays + ' Days Until ' + date2.toLocaleDateString() +'</h2>');
 };
 
 exports.event = (event, callback) => {
